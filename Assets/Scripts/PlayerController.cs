6130b913f241231c53be0414fae9250c11493767
ec21620 <146430352+ec21620@users.noreply.github.com>
2023-10-06 13:09:52 +0100

    Lab2-3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,10 +11,15 @@ public class PlayerController : MonoBehaviour
     public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 8;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
     void Start()
     {
         count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     void OnMove(InputValue value)
@@ -34,6 +41,16 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickups)
+        {
+            winText.text = "You Win!";
         }
     }
 }
